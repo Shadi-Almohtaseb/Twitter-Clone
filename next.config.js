794716f6541c08d9f46622f40c4e47288b07e0f7
Shadi-Almohtaseb <shadi876543211@gmail.com>
2023-01-28 16:39:56 +0200

    Fix deployment errors

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "firebasestorage.googleapis.com/v0/b/twitter-clone-3857e.appspot.com",
-    ],
+    domains: ["lh3.googleusercontent.com"],
   },
 };
